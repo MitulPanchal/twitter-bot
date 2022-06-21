ea94c990beaf0d75c3303b4f660e33cb008c9b9b
Mitul Panchal <imitulpanchal@gmail.com>
2022-06-21 12:03:32 +0530

    include search tweet with hashtag and retweet it

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import tweepy
 import tokens
+import time 
 
 auth = tweepy.OAuth1UserHandler(
    tokens.consumer_key_value, tokens.consumer_secret_value,
@@ -21,11 +22,22 @@ def store_last_seen(FILE_NAME, last_seen_id):
         file_write.write(str(last_seen_id))
     return 
 
-tweets = api.mentions_timeline(since_id = read_last_seen(FILE_NAME), tweet_mode = 'extended')
 
 
-for tweet in reversed(tweets):
-    if '#testtwitterapi' in tweet.full_text.lower():
-        print(str(tweet.id) + '-'+ tweet.full_text)
-        store_last_seen(FILE_NAME, tweet.id)
-        api.update_status(status = "@" + tweet.user.screen_name + " Message received! #iamcodernow", in_reply_to_status_id = tweet.id)
+def reply():
+    tweets = api.mentions_timeline(since_id = read_last_seen(FILE_NAME), tweet_mode = 'extended')
+    for tweet in reversed(tweets):
+        if '#testtwitterapi' in tweet.full_text.lower():
+            print("Replied to ID - " + str(tweet.id))
+
+            # Reply, retweet and like tweet 
+            api.update_status(status = "@" + tweet.user.screen_name + " Message received! #iamcodernow", in_reply_to_status_id = tweet.id)
+            api.create_favorite(tweet.id)
+            api.retweet(tweet.id)
+
+            # Update Last tweet ID bot reply to in file 
+            store_last_seen(FILE_NAME, tweet.id)
+
+while True:
+    reply()
+    time.sleep(15)        
